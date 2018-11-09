3dffef51d196d814de0d5903de2d726fa5c5c1ca
Erik Mauricio Moreno Vázquez <e.mauricio.moreno@gmail.com>
2018-11-09 12:57:20 -0600

    Se corrigió un bug

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ const APP_SHELL_INMUTABLE = [
 self.addEventListener('install', e => {
     const cacheStatic = caches.open(STATIC_CACHE).then( cache => cache.addAll(APP_SHELL));
     const cacheInmutable = caches.open(INMUTABLE_CACHE).then( cache => cache.addAll(APP_SHELL_INMUTABLE));
-    e.waitUnitl(Promise.all([cacheStatic, cacheInmutable]));
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
 
 self.addEventListener('activate', e => {
